c57032ff7d833cdda09a8ab00d3daeabd6273aba
mac1228 <mac1228@gmail.com>
2015-09-03 15:07:16 -0700

    added blink functionality

---
+++ Pod/Classes/BlinkingLabel.swift
@@ -0,0 +1,23 @@
+//
+//  BlinkingLabel.swift
+//  Pods
+//
+//  Created by Michael Curd on 9/3/15.
+//
+//
+
+import Foundation
+
+public class BlinkingLabel : UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .Repeat | .Autoreverse
+        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
